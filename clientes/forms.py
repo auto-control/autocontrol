27a5e51fd17106c5e85d569c76423ecdd38aacdc
Adriann Sanchez <alka65@hotmail.com>
2016-04-21 23:31:16 -0500

    number x

--- clientes/forms.py
+++ clientes/forms.py
@@ -8,11 +8,11 @@ class clienteModelForm(forms.ModelForm):
 		model = clienteModel
 		fields = '__all__'
 		widgets = {
-			'nombre': forms.TextInput(attrs={'required': True, 'class': 'form-control', 'maxlength': 35, 'pattern': '[A-Za-z]+', 'title': 'Digite solo letras'}),
+			'nombre': forms.TextInput(attrs={'required': True, 'class': 'form-control', 'maxlength': 35, 'pattern': '[A-Za-z ]+', 'title': 'Digite solo letras'}),
 			'documento': forms.TextInput(attrs={'class': 'form-control', 'pattern': '[0-9]{1,12}', 'title': 'Digite solo numeros'}),
-			'apellido': forms.TextInput(attrs={'required': True, 'class': 'form-control', 'maxlength': 35, 'pattern': '[A-Za-z]+', 'title': 'Digite solo numeros'}),
-			'telefono': forms.TextInput(attrs={'placeholder': 'xxxxxxxxxx', 'class': 'form-control', 'maxlength': 10, 'pattern': '[0-9]{1,10}', 'title': 'Digite solo numeros'}),
-			'celular': forms.TextInput(attrs={'placeholder': 'xxxxxxxxxx', 'class': 'form-control', 'maxlength': 10, 'pattern': '[0-9]{1,10}', 'title': 'Digite solo numeros'}),
-			'direccion': forms.TextInput(attrs={'placeholder': 'xxxxxxx', 'class': 'form-control'}),
+			'apellido': forms.TextInput(attrs={'required': True, 'class': 'form-control', 'maxlength': 35, 'pattern': '[A-Za-z ]+', 'title': 'Digite solo numeros'}),
+			'telefono': forms.TextInput(attrs={'placeholder': 'XXXXXXXXX', 'class': 'form-control', 'maxlength': 10, 'pattern': '[0-9]{1,10}', 'title': 'Digite solo numeros'}),
+			'celular': forms.TextInput(attrs={'placeholder': 'XXXXXXXXX', 'class': 'form-control', 'maxlength': 10, 'pattern': '[0-9]{1,10}', 'title': 'Digite solo numeros'}),
+			'direccion': forms.TextInput(attrs={'placeholder': 'XXXXXXXX', 'class': 'form-control'}),
 			'email': forms.EmailInput(attrs={'required': True, 'class': 'form-control'})
 		}
