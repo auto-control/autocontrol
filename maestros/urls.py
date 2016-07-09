d4bde647710e79fd65f403ac09011504dbe50272
Adriann Sanchez <alka65@hotmail.com>
2016-07-08 21:27:01 -0500

    ch

--- maestros/urls.py
+++ maestros/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 
 from .views import *
 
+
 urlpatterns = [
 	url(r'^mecanicos/$', listMecanicosView.as_view(), name = 'list_mecanicos'),
 	url(r'^mecanicos/nuevo$', createMecanicoView.as_view(), name = 'create_mecanicos'),
