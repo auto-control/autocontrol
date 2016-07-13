6021a13172933ae1aedfc45d2144c48ee8b31383
Adriann Sanchez <alka65@hotmail.com>
2016-07-12 23:09:28 -0500

    deploy heroku

--- auto_control/settings.py
+++ auto_control/settings.py
@@ -150,12 +150,22 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = '/media/'
 
-"""
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL ='/img/'
+
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'static'),
+)
+"""
+
+STATIC_ROOT = 'static'
+
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'static')
 
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, 'static'),
