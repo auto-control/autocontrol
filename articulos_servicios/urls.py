8885b55671083c910708425d136213961ae4c237
Adriann Sanchez <alka65@hotmail.com>
2016-08-13 19:39:44 -0500

    repuestos

--- articulos_servicios/urls.py
+++ articulos_servicios/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
-from articulos_servicios.views import createServicioView, listServicioView
+from articulos_servicios.views import *
 
 urlpatterns = [
 	url(r'^servicios/nuevo$', createServicioView.as_view(), name = 'create_servicio'),
 	url(r'^servicios/$', listServicioView.as_view(), name = 'list_servicio'),
+	url(r'^servicios/actualizar/(?P<pk>\d+)/$', updateServicioView.as_view(), name = 'update_servicios')
 ]
--- articulos_servicios/views.py
+++ articulos_servicios/views.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView
 from articulos_servicios.forms import servicioModelForm
 from articulos_servicios.models import servicioModel
 from django.utils.decorators import method_decorator
@@ -14,6 +14,16 @@ class createServicioView(CreateView):
 	@method_decorator(user_passes_test(lambda u: u.usuariosmodel.tipoUsuario.nombre_tipo == 'Administrador', login_url='/'))
 	def dispatch(self, *args, **kwargs):
 		return super(createServicioView,  self).dispatch(*args, **kwargs)
+
+class updateServicioView(UpdateView):
+	model = servicioModel
+	form_class = servicioModelForm
+	template_name = 'create_servicio.html'
+	success_url = '/servicios'
+
+	@method_decorator(user_passes_test(lambda u: u.usuariosmodel.tipoUsuario.nombre_tipo == 'Administrador', login_url='/'))
+	def dispatch(self, *args, **kwargs):
+		return super(updateServicioView,  self).dispatch(*args, **kwargs)
 
 class listServicioView(ListView):
 	template_name = 'list_servicio.html'
